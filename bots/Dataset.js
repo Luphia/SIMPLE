980c1e4f604149703b2490a2f6e8d9f6cd283426
hmgamer <hmgamer@yahoo.com.tw>
2015-03-26 12:19:29 +0800

    Dataset的各REST方法，增加其相對應的message。

--- bots/Dataset.js
+++ bots/Dataset.js
@@ -56,7 +56,7 @@ Bot.prototype.exec = function (msg) {
 	var rs = new Result();
 	var url = msg.url;
 	var body = msg.body;
-	var pass, uri, table, sql, query, id, rsdata, info;
+	var pass, uri, table, sql, query, id, rsdata, info, message;
 
 	if(url) {
 		pass = (msg.method == 'GET' && (url.lastIndexOf('/') == url.length - 1) ? 'LIST' : msg.method) + url.split('/').length.toString();
@@ -74,63 +74,79 @@ Bot.prototype.exec = function (msg) {
 		case 'newDataset':
 			var label = msg.query.label;
 			rsdata = this.newDataset(msg.body, label);
+			message = "Set new "+msg.body+" Dataset Successful";
 			break;
 
 		case 'FIND':
 			rsdata = this.db.find(table, query);
+			message = "Find Table Data Successful";
 			break;
 		case 'LIST3':
 			if (sql) {
 				rsdata = this.db.sql(req, res, next);
+				message = "SQL Execute Successful";
 			}
 			else {
 				rsdata = this.db.listTable();
+				message = "List All Table Successful";
 			}
 			break;
 		case 'GET3':
 			rsdata = this.db.getTable(table);
+			message = "Get Table Schema Successful";
 			break;
 		case 'POST3':
 			var schema = body;
 			rsdata = this.db.postTable(table, schema);
+			message = "Create Table Successful";
 			break;
 		case 'PUT3':
 			var schema = body;
 			rsdata = this.db.putTable(table, schema);
+			message = "Modify Table Successful";
 			break;
 		case 'DELETE3':
 			rsdata = this.db.deleteTable(table);
+			message = "Delete Table Successful";
 			break;
 		case 'LIST4':
 			rsdata = this.db.pageData(table, query);
+			message = "List Data to "+table+" Table Successful";
 			break;
 		case 'GET4':
 			rsdata = this.db.getData(table, id);
+			message = "Get Data from "+table+" Table Successful";
 			break;
 		case 'POST4':
 			var data = body;
 			rsdata = this.db.postData(table, body);
+			message = "Insert Data to "+table+" Table Successful";
 			break;
 		case 'PUT4':
 			var data = body;
 			if (query) {
 				rsdata = this.db.updateData(table, id, data);
+				message = "Update Data to "+table+" Table Successful";
 			}
 			else {
 				rsdata = this.db.putData(table, id, data);
+				message = "Update or Insert Data to "+table+" Table Successful";
 			}
 			break;
 		case 'DELETE4':
 			rsdata = this.db.deleteData(table, id);
+			message = "Delete Data to "+table+" Table Successful";
 			break;
 
 		default:
 			rsdata = msg;
+			message = "Debug Message";
 			break;
 	}
 
 	if(rsdata) {
 		rs.setResult(1);
+		rs.setMessage(message);
 		rs.setData(rsdata);
 	}
 
