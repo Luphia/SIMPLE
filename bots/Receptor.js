574943632825352f65511547cc42c1e945ed70e7
Luphia Chang <luphia@outlook.com>
2015-04-09 18:33:50 +0800

    Add jieba to library

--- bots/Receptor.js
+++ bots/Receptor.js
@@ -1,5 +1,6 @@
 var SocketBot = require('./_SocketBot.js')
 ,	util = require('util')
+,	log4js = require('log4js')
 ,	express = require('express')
 ,	Session = require('express-session')
 ,	favicon = require('serve-favicon')
@@ -23,6 +24,20 @@ Receptor.prototype.init = function(config) {
 	if (!fs.existsSync(upload)){
 		fs.mkdirSync(upload);
 	}
+	var logs = "./logs/";
+	if (!fs.existsSync(logs)){
+		fs.mkdirSync(logs);
+	}
+
+	log4js.configure({
+		"appenders": [
+			{ "type": "console" },
+			{ "type": "dateFile", "filename": "./logs/catering", "category": "catering.log", "pattern": "-yyyy-MM-dd.log", "alwaysIncludePattern": true, "backups": 365 },
+			{ "type": "file", "filename": "./logs/catering.exception.log", "category": "catering.exception", "maxLogSize": 10485760, "backups": 10 },
+			{ "type": "file", "filename": "./logs/catering.threat.log", "category": "catering.threat", "maxLogSize": 10485760, "backups": 10 }
+		],
+		"replaceConsole": true
+	});
 
 	this.router = express.Router();
 	this.app = express();
@@ -34,6 +49,7 @@ Receptor.prototype.init = function(config) {
 	});
 
 	this.app.set('port', 80);
+	this.app.use(log4js.connectLogger(log4js.getLogger('catering.log'), { level: log4js.levels.INFO, format: ':remote-addr :user-agent :method :url :status - :response-time ms' }));
 	this.app.use(this.session);
 	this.app.use(bodyParser.urlencoded({ extended: false }));
 	this.app.use(bodyParser.json({limit: '100mb'}));
@@ -108,7 +124,7 @@ Receptor.prototype.stop = function() {
 };
 
 Receptor.prototype.filter = function(req, res, next) {
-	res.result = {"result": 0, "message": "", data: {}};
+	res.result = new Result();
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
 	next();
