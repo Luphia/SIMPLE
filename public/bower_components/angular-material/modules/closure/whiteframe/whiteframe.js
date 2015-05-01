db0a5374d320d90cbfd5bd308ae62c0554b0d925
Luphia Chang <luphia@outlook.com>
2015-05-01 16:55:24 +0800

    Add UI library

---
+++ public/bower_components/angular-material/modules/closure/whiteframe/whiteframe.js
@@ -0,0 +1,17 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.9.0-rc2-master-bed14b9
+ */
+goog.provide('ng.material.components.whiteframe');
+
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+ng.material.components.whiteframe = angular.module('material.components.whiteframe', []);
+})();
---
+++ public/bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.9.0-rc2-master-bed14b9
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
