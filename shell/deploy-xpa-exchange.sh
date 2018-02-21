f3b76e9e376638cc559e1e68045cb0ac8cb7c1fd
Luphia Chang <luphiaccw@gmail.com>
2018-02-21 18:10:10 +0800

    Update deploy-xpa-exchange.sh

--- shell/deploy-xpa-exchange.sh
+++ shell/deploy-xpa-exchange.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TRUFFLE_PATH = $(sudo npm i -g truffle | grep truffle | cut -d ' ' -f 1)
+TRUFFLE_PATH=$(sudo npm i -g truffle | grep truffle | cut -d ' ' -f 1)
 sudo ln -s $TRUFFLE_PATH /usr/local/bin
 echo $TRUFFLE_PATH
 cd ~
