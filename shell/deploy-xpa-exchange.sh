9b708074157ac1b7daffe91cec1e460b30fe3069
Luphia Chang <luphiaccw@gmail.com>
2018-02-21 19:31:14 +0800

    Update deploy-xpa-exchange.sh

--- shell/deploy-xpa-exchange.sh
+++ shell/deploy-xpa-exchange.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TRUFFLE_PATH=$(sudo npm i -g truffle | grep truffle | cut -d ' ' -f 1) | grep truffle
+TRUFFLE_PATH=$(sudo npm i -g truffle | grep truffle | cut -d ' ' -f 1 | grep truffle)
 sudo ln -s $TRUFFLE_PATH /usr/local/bin
 cd ~
 wget http://cc-wei.com/release/baliv-offchain.tar
