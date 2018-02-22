8b894574175f7c26905032c2c2ecdb359b3853a6
Luphia Chang <luphiaccw@gmail.com>
2018-02-22 14:27:04 +0800

    Update deploy-xpa-exchange.sh

--- shell/deploy-xpa-exchange.sh
+++ shell/deploy-xpa-exchange.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-TRUFFLE_PATH=$(sudo npm i -g truffle | grep truffle | cut -d ' ' -f 1 | grep truffle)
-sudo ln -s $TRUFFLE_PATH /usr/local/bin
 cd ~
-wget http://cc-wei.com/release/baliv-offchain.tar
-tar xvf baliv-offchain.tar
-cd baliv-offchain
-npm i
+mkdir geth-xpaexchange
+cd geth-xpaexchange
+PW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+PUBLIC_KEY=$((geth --datadir ~/geth-xpaexchange/ account new << !
+$PW
+$PW
+!) | grep Address | cut -d '{' -f 2 | cut -d '}' -f 1 )
+echo $PW > .pw
+echo '
 {
-  truffle migrate --network parity
+"alloc" : { "$PUBLIC_KEY": { "balance": "0x33b2e3c9fd0804000000000" } },
+"coinbase" : "0x$PUBLIC_KEY",
+"difficulty" : "0x2000",
+"extraData" : "",
+"gasLimit" : "0x5d8b80",
+"nonce" : "0x0000000000000000",
+"mixhash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
+"parentHash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
+"timestamp" : "0x00",
+"config": {
+"chainId": 1,
+"homesteadBlock": 0,
+"eip155Block": 0,
+"eip158Block": 0
 }
-nohup node . &
+}
+' > ./genesis.json
+geth --datadir ~/geth-xpaexchange init ~/geth-xpaexchange/genesis.json
+nohup geth --mine --minerthreads=4 --maxpeers 10 --cache=2048 --datadir ~/geth-xpaexchange/ --rpc --rpcaddr 0.0.0.0 --rpcport 8745 --rpcapi db,eth,net,web3,personal --rpccorsdomain * --etherbase $PUBLIC_KEY --unlock 0x$PUBLIC_KEY --password ~/baliv-geth/.pw --port 30301 &
