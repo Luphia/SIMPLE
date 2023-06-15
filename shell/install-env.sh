421b74e6985838ac0f69dd05d60a703a1e632e16
Luphia Chang <luphiaccw@gmail.com>
2023-06-15 16:54:16 +0800

    Update install-env.sh

--- shell/install-env.sh
+++ shell/install-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#NODE_FULLFILENAME=$(curl https://nodejs.org/dist/latest/SHASUMS256.txt | grep linux-x64.tar.gz | cut -d ' ' -f 3)
-#NODE_VERSION=$(echo $NODE_FULLFILENAME | cut -d '-' -f 2)
-NODE_VERSION="v11.9.0"
+NODE_FULLFILENAME=$(curl https://nodejs.org/dist/latest/SHASUMS256.txt | grep linux-x64.tar.gz | cut -d ' ' -f 3)
+NODE_VERSION=$(echo $NODE_FULLFILENAME | cut -d '-' -f 2)
 NODE_FILENAME="node-$NODE_VERSION-linux-x64"
 PARENT_LOCATION="/opt/nodejs"
 
@@ -25,4 +24,8 @@ sudo ln -s $PARENT_LOCATION/$NODE_FILENAME/bin/node /usr/local/bin
 sudo ln -s $PARENT_LOCATION/$NODE_FILENAME/bin/npm /usr/local/bin
 
 ### assign 80 & 443 port ###
-sudo setcap 'cap_net_bind_service=+ep' $PARENT_LOCATION/$NODE_FILENAME/bin/node
+sudo setcap cap_net_bind_service=+ep /opt/nodejs/$NODE_FILENAME/bin/node
+
+### Install PM2
+sudo npm install -g pm2
+sudo ln -s /opt/nodejs/node-$NODE_VERSION-linux-x64/lib/node_modules/pm2/bin/pm2 /usr/local/bin/
